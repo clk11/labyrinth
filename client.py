2ed7d1d44d25cbfe2bbcb81b5729df031c58cba6
= <trasca.robert2003@gmail.com>
2024-01-14 20:29:00 +0200

    socket version

--- client.py
+++ client.py
@@ -1,10 +1,54 @@
 import socket
-
+import threading
+import os
+import pickle
+import time
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("mothership ip", 5050))
+client_socket.connect(("172.30.100.205", 5018))
 
-while True:
-    inp = input("Introduce the message to send : ")
-    if inp == "q":
-        break
-    client_socket.send(inp.encode('utf-8'))
+stop = False
+
+
+def display_lab(current):
+    for i in range(current['rows']):
+        for j in range(current['cols']):
+            if i == current['playerX'] and j == current['playerY']:
+                print("🎅 ", end='')
+            elif i == current['monsterX'] and j == current['monsterY']:
+                print("😈 ", end='')
+            else:
+                print(current['matrix'][i][j] + "  ", end='')
+        print()
+
+
+def display_thread():
+    while True:
+        received_data = client_socket.recv(4096)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        current = pickle.loads(received_data)
+        if 'status' in current:
+            print(current['message'])
+            client_socket.close()
+            os._exit(0)
+        display_lab(current)
+
+
+def input_thread():
+    while True:
+        user_input = input()
+        client_socket.send(user_input.encode('utf-8'))
+        if user_input.lower() == 'quit':
+            print("Revin-o oricand 😉 !")
+            os._exit(0)
+
+
+input_handler = threading.Thread(target=input_thread)
+display_handler = threading.Thread(target=display_thread)
+
+input_handler.start()
+display_handler.start()
+
+input_handler.join()
+display_handler.join()
+
+client_socket.close()
--- server.py
+++ server.py
@@ -1,26 +1,156 @@
 import socket
 import threading
+import random
+import time
+import pickle
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("mothership ip", 5050))
+server_socket.bind(("172.30.100.205", 5018))
 server_socket.listen(5)
 
+
+class Game:
+    def __init__(self, rows, cols, client_socket, addr):
+        self.stop = False
+        self.addr = addr
+        self.client_socket = client_socket
+        self.rows = rows
+        self.cols = cols
+        self.exitX = 0
+        self.exitY = 0
+        self.playerX = 0
+        self.playerY = 0
+        self.monsterX = 0
+        self.monsterY = 0
+        self.matrix = [['' for _ in range(self.cols)]
+                       for _ in range(self.rows)]
+        random_number = random.randint(1, 5)
+        path = "map"+str(random_number)+".txt"
+        with open(path, 'r') as file:
+            for row_index in range(self.rows):
+                line = file.readline().strip()
+                for col_index in range(self.cols):
+                    if line[col_index] == "E":
+                        self.exitX = row_index
+                        self.exitY = col_index
+                        self.matrix[row_index][col_index] = " "
+                    elif line[col_index] == "H":
+                        self.playerX = row_index
+                        self.playerY = col_index
+                        self.matrix[row_index][col_index] = "."
+                    elif line[col_index] == "M":
+                        self.monsterX = row_index
+                        self.monsterY = col_index
+                        self.matrix[row_index][col_index] = "."
+                    else:
+                        self.matrix[row_index][col_index] = line[col_index]
+
+    def caught(self):
+        check = self.playerX == self.monsterX and self.playerY == self.monsterY
+        if check:
+            self.stop = True
+            game_state = {
+                'status': 'end',
+                'message': 'Ai picat pradă monstrului din labirint 🙁… ai pierdut jocul. Încerca din nou!'
+            }
+            object_copy = pickle.dumps(game_state)
+            client_socket.send(object_copy)
+        return check
+
+    def exit(self):
+        check = self.playerX == self.exitX and self.playerY == self.exitY
+        if check:
+            self.stop = True
+            game_state = {
+                'status': 'end',
+                'message': 'Ai iesit !'
+            }
+            object_copy = pickle.dumps(game_state)
+            client_socket.send(object_copy)
+        return check
+
+    def check_obstacle(self, x, y):
+        return self.matrix[x][y] != '#'
+
+    def preventEscape(self, x, y):
+        return x == self.exitX and y == self.exitY
+
+    def monster_movement(self):
+        while self.stop != True:
+            game_state = {
+                'playerX': self.playerX,
+                'playerY': self.playerY,
+                'monsterX': self.monsterX,
+                'monsterY': self.monsterY,
+                'rows': self.rows,
+                'cols': self.cols,
+                'matrix': self.matrix,
+            }
+            object_copy = pickle.dumps(game_state)
+            client_socket.send(object_copy)
+            if (self.caught()):
+                break
+            if self.exit():
+                break
+            random_move = random.choice(['w', 'a', 'd', 's'])
+            if random_move == 'w':
+                if self.check_obstacle(self.monsterX - 1, self.monsterY) and self.preventEscape(self.monsterX - 1, self.monsterY) == False:
+                    self.monsterX -= 1
+            elif random_move == 'a':
+                if self.check_obstacle(self.monsterX, self.monsterY - 1) and self.preventEscape(self.monsterX, self.monsterY-1) == False:
+                    self.monsterY -= 1
+            elif random_move == 'd':
+                if self.check_obstacle(self.monsterX, self.monsterY + 1) and self.preventEscape(self.monsterX, self.monsterY+1) == False:
+                    self.monsterY += 1
+            elif random_move == 's':
+                if self.check_obstacle(self.monsterX + 1, self.monsterY) and self.preventEscape(self.monsterX+1, self.monsterY) == False:
+                    self.monsterX += 1
+            time.sleep(0.21)
+
+    def movement(self):
+        while self.stop != True:
+            key_pressed = self.client_socket.recv(1024).decode('utf-8')
+            if key_pressed.lower() == 'quit':
+                self.stop = True
+                break
+            if key_pressed == 'w':
+                if self.check_obstacle(self.playerX - 1, self.playerY):
+                    self.playerX -= 1
+            elif key_pressed == 'a':
+                if self.check_obstacle(self.playerX, self.playerY - 1):
+                    self.playerY -= 1
+            elif key_pressed == 'd':
+                if self.check_obstacle(self.playerX, self.playerY + 1):
+                    self.playerY += 1
+            elif key_pressed == 's':
+                if self.check_obstacle(self.playerX + 1, self.playerY):
+                    self.playerX += 1
+
+    def start_monster_movement_thread(self):
+        monster_movement_thread = threading.Thread(
+            target=self.monster_movement)
+        monster_movement_thread.start()
+
+    def start_player_movement_thread(self):
+        player_movement_thread = threading.Thread(target=self.movement)
+        player_movement_thread.start()
+
+
 def handle_client(client_socket, addr):
-    print(f"{addr} joined the chat !")
+    print(f"Player {addr} started a game !")
+    game = Game(10, 10, client_socket, addr)
+    game.start_monster_movement_thread()
+    game.start_player_movement_thread()
+
+    game_movement_thread = threading.Thread(target=game.movement)
+    game_movement_thread.start()
+    game_movement_thread.join()
+    print(f"Player {addr} finished the game !")
+
+
+if __name__ == "__main__":
     while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        decoded_data = data.decode('utf-8')
-        print(f"{addr} says: {decoded_data}")
-
-    print(f"Connection from {addr} closed.")
-    client_socket.close()
-
-
-print("Server is listening for incoming connections.")
-
-while True:
-    client_socket, addr = server_socket.accept()
-    client_handler = threading.Thread(target=handle_client, args=(client_socket, addr))
-    client_handler.start()
+        client_socket, addr = server_socket.accept()
+        client_handler = threading.Thread(
+            target=handle_client, args=(client_socket, addr))
+        client_handler.start()
